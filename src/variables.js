108a55b5ef33fb86b3b9ef1a75143a437423d004
U-CORP\swright <swright@V07C29D04123.corp.intuit.net>
2015-01-29 16:41:43 -0600

    Fix variables.js

--- src/variables.js
+++ src/variables.js
@@ -14,7 +14,9 @@ function variableModification(a) {
   var asString;
   var yourNumberIs;
   //your code here
-
+   plus5 = a + 5;
+   asString = String(a);
+   yourNumberIs = "Your Number is " + a;
   //end your code
   return [plus5, asString, yourNumberIs, a];
 }
@@ -29,7 +31,13 @@ false otherwise
 */
 function isString(b) {
   //your code here
-  return undefined;
+  if (typeof b == 'string')
+  { 
+	return true;
+  } else
+  {
+  return false;
+  }
   //end your code
 }
 
@@ -42,6 +50,17 @@ return true if c is null, false otherwise
 */
 function isNull(c) {
   //your code here
-  return undefined;
+  if ((c != null))
+  { 
+	return false;
+  } 
+    if (typeof c == 'undefined')
+  { 
+	return false;
+  } 
+  if ((c == null))
+  {
+	return true;
+  }
   //end your code
 }
